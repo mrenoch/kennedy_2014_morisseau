aef7b96f0f64be733d971ac43d3bdb845874db8f
Rachelle <rwhite@columbia.edu>
2014-09-24 12:34:46 -0400

    added a gallery

--- js/init.js
+++ js/init.js
@@ -62,6 +62,20 @@
 		// Scrolly links.
 			$('.scrolly').scrolly();
 
-	});
+	
+	
+		// Poptrox.
+			$('.gallery').poptrox({
+				useBodyOverflow: false,
+				usePopupEasyClose: false,
+				overlayColor: '#0a1919',
+				overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
+				usePopupDefaultStyling: false,
+				usePopupCaption: true,
+				popupLoaderText: '',
+				windowMargin: 10,
+				usePopupNav: true
+			});
+});
 
 })(jQuery);
